b42b4e0e7dff5f4be9dc8b4800ce264c25e2f814
Nathan Dorsey <421420+glycerine102@users.noreply.github.com>
2026-05-06 14:45:54 -0400

    style: edit comments to follow same styling

--- build_files/zfs.sh
+++ build_files/zfs.sh
@@ -2,19 +2,17 @@
 
 set -eoux pipefail
 
-### aurora 02-install-common-kernel-akmods.sh ###
 # Remove Existing Kernel, mostly whatever `rpm -qa | grep kernel` spits out
 for pkg in kernel kernel{-core,-modules,-modules-core,-modules-extra}; do
     rpm --erase "${pkg}" --nodeps
 done
 
-# probably should remove the kmods as well just to be sure
-# as they have to be built for the exact kernel version
+# Remove the kmods as well as they have to be built for the exact kernel version
 for pkg in kmod-xone xone-kmod-common kmod-v4l2loopback v4l2loopback; do
     rpm --erase "${pkg}" --nodeps
 done
 
-# cleanup leftovers that are not covered by kernel-* packages for some reason
+# Cleanup leftovers that are not covered by kernel-* packages for some reason
 rm -rf /usr/lib/modules
 
 # Install Kernel
@@ -23,15 +21,15 @@ dnf5 -y install \
     /tmp/kernel-rpms/kernel-core-*.rpm \
     /tmp/kernel-rpms/kernel-modules-*.rpm
 
+# Versionlock kernel packages to prevent them from being updated
 dnf5 versionlock add kernel kernel-devel kernel-devel-matched kernel-core kernel-modules kernel-modules-core kernel-modules-extra
 
-# if you care about this kinda stuff
+# Install these packages back in
 dnf -y install /tmp/rpms/{common,kmods}/*xone*.rpm
 dnf -y install /tmp/rpms/{kmods,common}/*v4l2loopback*.rpm
 
 mkdir -p /etc/pki/akmods/certs
 curl "https://github.com/ublue-os/akmods/raw/refs/heads/main/certs/public_key.der" --retry 3 -Lo /etc/pki/akmods/certs/akmods-ublue.der
-### aurora 02-install-common-kernel-akmods.sh ###
 
 KERNEL=$(basename $(find /usr/lib/modules -maxdepth 1 -type d | tail -n 1))
 
